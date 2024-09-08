5a021e636b3b20c24a394f132acf0324378a7e31
Kingviphack <kvishal72892@gmail.com>
2024-09-08 11:55:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7347948044:AAGqD0pEB8PpufQ0uB5GqNZBlAs-3X7oDQ0")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@bgmistored")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7130327403,6317341906")
 
 
 
